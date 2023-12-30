10e06de4765eed0b6bf90a83f0ec1adeea441b49
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2023-12-30 08:44:33 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const mypath =
+  /* GET home page. */
+  router.get("/", function (req, res, next) {
+    res.sendFile.path.join(__dirname, "index.html");
+  });
 
 module.exports = router;
